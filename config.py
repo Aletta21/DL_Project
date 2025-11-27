491636becf78481520ec295d7667f58eff5984b1
Aletta21 <s252976@dtu.dk>
2025-11-27 13:53:28 +0100

    Commented Path in config

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ from pathlib import Path
 # Base directory containing the AnnData files.
 DATA_ROOT = Path("/work3/s193518/scIsoPred/data").expanduser().resolve()
 
-DEFAULT_GENE_H5AD = str(DATA_ROOT / "bulk_processed_genes.h5ad")
-DEFAULT_ISOFORM_H5AD = str(DATA_ROOT / "bulk_processed_transcripts.h5ad")
+DEFAULT_GENE_H5AD = str(DATA_ROOT / "bulk_processed_genes.h5ad") #sc-processed_genes.h5ad
+DEFAULT_ISOFORM_H5AD = str(DATA_ROOT / "bulk_processed_transcripts.h5ad") #sc-processed_transcripts.h5ad
